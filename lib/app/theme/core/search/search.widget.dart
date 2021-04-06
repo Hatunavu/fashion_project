0527bd054dbb5b0167cc374e37f6ee9e982145ab
Hatunavu <vutruongnam0@gmail.com>
2021-04-06 09:29:40 +0700

    abc

--- lib/app/theme/core/search/search.widget.dart
+++ lib/app/theme/core/search/search.widget.dart
@@ -12,92 +12,52 @@ class _SearchPageState extends State<SearchWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: widget.notBack == false
-          ? AppBar(
-              backgroundColor: Colors.white,
-              leading: InkWell(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Icon(
-                  Icons.arrow_back,
-                  color: Colors.black,
-                ),
-              ),
-              automaticallyImplyLeading: false,
-              elevation: 0,
-              brightness: Brightness.light,
-              centerTitle: true,
-              titleSpacing: 0,
-              title: Container(
-                height: 35,
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10.0,
-                ),
-                margin:
-                    EdgeInsets.only(right: 15, top: 10, bottom: 10, left: 15),
-                alignment: Alignment.centerLeft,
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(244, 243, 243, 1),
-                  borderRadius: BorderRadius.all(Radius.circular(50)),
-                ),
-                child: TextField(
-                  textAlign: TextAlign.left,
-                  strutStyle: StrutStyle(fontSize: 16, height: 1.5),
-                  autocorrect: false,
-                  style:
-                      TextStyle(height: 1.5, fontSize: 14, color: Colors.black),
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Search you\'re looking for',
-                      hintStyle: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black.withOpacity(.4)),
-                      icon: Icon(
-                        CustomIcons.icon_search,
-                        size: 20,
-                      )),
-                ),
+        appBar: AppBar(
+      backgroundColor: Colors.white,
+      leading: widget.notBack == false
+          ? InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Icon(
+                Icons.arrow_back,
+                color: Colors.black,
               ),
             )
-          : AppBar(
-              backgroundColor: Colors.white,
-              automaticallyImplyLeading: false,
-              elevation: 0,
-              brightness: Brightness.light,
-              centerTitle: true,
-              titleSpacing: 0,
-              title: Container(
-                height: 35,
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10.0,
-                ),
-                margin:
-                    EdgeInsets.only(right: 15, top: 10, bottom: 10, left: 15),
-                alignment: Alignment.centerLeft,
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(244, 243, 243, 1),
-                  borderRadius: BorderRadius.all(Radius.circular(50)),
-                ),
-                child: TextField(
-                  textAlign: TextAlign.left,
-                  strutStyle: StrutStyle(fontSize: 16, height: 1.5),
-                  autocorrect: false,
-                  style:
-                      TextStyle(height: 1.5, fontSize: 14, color: Colors.black),
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Search you\'re looking for',
-                      hintStyle: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black.withOpacity(.4)),
-                      icon: Icon(
-                        CustomIcons.icon_search,
-                        size: 20,
-                      )),
-                ),
-              ),
-            ),
-    );
+          : null,
+      automaticallyImplyLeading: widget.notBack == false ? true : false,
+      elevation: 0,
+      brightness: Brightness.light,
+      centerTitle: true,
+      titleSpacing: 0,
+      title: Container(
+        height: 35,
+        padding: EdgeInsets.symmetric(
+          horizontal: 10.0,
+        ),
+        margin: EdgeInsets.only(right: 15, top: 10, bottom: 10, left: 15),
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(244, 243, 243, 1),
+          borderRadius: BorderRadius.all(Radius.circular(50)),
+        ),
+        child: TextField(
+          textAlign: TextAlign.left,
+          strutStyle: StrutStyle(fontSize: 16, height: 1.5),
+          autocorrect: false,
+          style: TextStyle(height: 1.5, fontSize: 14, color: Colors.black),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Search you\'re looking for',
+              hintStyle: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black.withOpacity(.4)),
+              icon: Icon(
+                CustomIcons.icon_search,
+                size: 20,
+              )),
+        ),
+      ),
+    ));
   }
 }
--- lib/app/theme/home/components/product.home.dart
+++ lib/app/theme/home/components/product.home.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../../../custom_icons_icons.dart';
+
 class ProductHome extends StatefulWidget {
+  final bool notBack;
+  ProductHome({this.notBack});
   @override
   _ProductHomeState createState() => _ProductHomeState();
 }
@@ -9,10 +13,52 @@ class _ProductHomeState extends State<ProductHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0.5,
+        appBar: AppBar(
+      backgroundColor: Colors.white,
+      leading: widget.notBack == false
+          ? InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Icon(
+                Icons.arrow_back,
+                color: Colors.black,
+              ),
+            )
+          : null,
+      automaticallyImplyLeading: widget.notBack == false ? true : false,
+      elevation: 0,
+      brightness: Brightness.light,
+      centerTitle: true,
+      titleSpacing: 0,
+      title: Container(
+        height: 35,
+        padding: EdgeInsets.symmetric(
+          horizontal: 10.0,
+        ),
+        margin: EdgeInsets.only(right: 15, top: 10, bottom: 10, left: 15),
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(244, 243, 243, 1),
+          borderRadius: BorderRadius.all(Radius.circular(50)),
+        ),
+        child: TextField(
+          textAlign: TextAlign.left,
+          strutStyle: StrutStyle(fontSize: 16, height: 1.5),
+          autocorrect: false,
+          style: TextStyle(height: 1.5, fontSize: 14, color: Colors.black),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Search you\'re looking for',
+              hintStyle: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black.withOpacity(.4)),
+              icon: Icon(
+                CustomIcons.icon_search,
+                size: 20,
+              )),
+        ),
       ),
-    );
+    ));
   }
 }
